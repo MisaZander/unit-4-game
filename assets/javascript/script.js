c60793610e40593af4c61404c0fd14c384df0241
Alexander Osborn <misterzander@gmail.com>
2018-09-02 15:14:17 -0500

    Complete stage 1 minus the click events

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,25 +8,25 @@ var game = {
          HP: 120,
          BAP: 20,
          CAP: 15,
-         img: "../images/BM.png"},
+         img: "assets/images/BM.png"},
         {name: "Random Fedora",
          player: false,
          HP: 100,
          BAP: 30,
          CAP: 20,
-         img: "../images/fedora.png"},
+         img: "assets/images/fedora.png"},
         {name: "Bachelor Party",
          player: false,
          HP: 140,
          BAP: 15,
          CAP: 25,
-         img: "../images/BP.png"},
+         img: "assets/images/BP.png"},
         {name: "Stretchy Death",
          player: false,
          HP: 150,
          BAP: 35,
          CAP: 30,
-         img: "../images/SD.png"}
+         img: "assets/images/SD.png"}
     ],
 
     init: function() {
@@ -36,9 +36,40 @@ var game = {
     },
 
     buildChars: function() {
-        //Create all the character divs and hide them
+        //Create all the character divs, add them, and hide them
         for(let i = 0; i < game.players.length; i++){
-            
+            var newCard = $("<div>");
+            newCard.attr("id", "player" + i);
+            newCard.attr("class", "char-card");
+
+            //Create the top of the card, the name div
+            var nameDiv = $("<div>");
+            nameDiv.attr("class", "char-text");
+            var name = $("<p>");
+            name.attr("class", "player-text");
+            name.text(game.players[i].name);
+            nameDiv.append(name);
+            newCard.append(nameDiv);
+
+            //Add the image to the card
+            var imgDiv = $("<div>");
+            imgDiv.attr("class", "char-img");
+            var image = $("<img>");
+            image.attr("src", game.players[i].img);
+            imgDiv.append(image);
+            newCard.append(imgDiv);
+
+            //Finally, add the stats to the bottom
+            var statDiv = $("<div>");
+            statDiv.attr("class", "char-text");
+            var stat = $("<p>");
+            stat.attr("class", "player-text");
+            stat.text("HP: " + game.players[i].HP);
+            statDiv.append(stat);
+            newCard.append(statDiv);
+
+            //Tack the new character card into player selection
+            $("#humans").append(newCard);
         }
     },
 
@@ -46,6 +77,10 @@ var game = {
         //This function will fire in response to primarly click events
         switch(this.stage) {
             case 1: //Start a fresh game
+                for(let i = 0; i < game.players.length; i++){
+                    //Reveal the players
+                    $("#player" + i).css("display", "block");
+                }
                 break;
             default:
                 alert("Oops, something went wrong! Please reload the page and fail the programmer responsible.");
